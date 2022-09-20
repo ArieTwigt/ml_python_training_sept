7de19a8da8b009307d18892aa8c04800531cd42d
Arie Twigt <ariekanarie@twigtd4.nl>
2022-09-20 13:42:08 +0200

    added example for logistic regression

--- 4_logistic_regression.py
+++ 4_logistic_regression.py
@@ -2,7 +2,9 @@
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, roc_curve, roc_auc_score
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 #%% import the dataset
 df = pd.read_csv("data/german_data_clean.csv")
@@ -14,7 +16,7 @@ df.head()
 df['response'].value_counts()
 
 # %%
-x_columns = ['age_years', 'credit_amount', 'duration_months', 'purpose', 'property']
+x_columns = ['age_years', 'credit_amount', 'duration_months', 'purpose', 'property', 'credit_history', 'housing', 'job']
 y_column = ['response']
 
 #%% create a data frame that only contains the x-variables
@@ -58,5 +60,29 @@ y_pred = df_preds['predicted']
 
 #%% create confusion matrix
 cf_matrix = confusion_matrix(y_test, y_pred)
+
+# %% visualize the confustion matrix
+ax = sns.heatmap(cf_matrix, annot=True, cmap='Blues', fmt='g')
+ax.set_title(f"Confusion Matrix total: ({len(y_test)})")
+ax.set_xlabel("Predicted values")
+ax.set_ylabel("Actual values")
+
+# %% calculate the metrics
+model_accuracy = accuracy_score(y_test, y_pred) # (199 + 20) / (300)
+model_precision = precision_score(y_test, y_pred) # (20 / (20 + 15)) how serious can we take a 'positive' prediction from the model
+model_recall = recall_score(y_test, y_pred, pos_label=1) # (20 / 86) how good is our model in identifying the positive class
+model_specificity = recall_score(y_test, y_pred, pos_label=0) # (199 / (199 + 15)) how good is our model in identifiying the negative class
+
+
+# %%  create the ROC-curve
+fpr, tpr, _ = roc_curve(y_test, y_pred_proba[:, 1]) # function returns 3 values/objects, the '_' is a placeholder
+
+#%%
+current_auc_score = roc_auc_score(y_test, y_pred_proba[:, 1])
+
+# %%
+plt.plot(fpr, tpr, label=f"AUC {str(current_auc_score)}")
+plt.plot([0,1], [0,1], linestyle="--", lw=2, color='r', label='Random guess')
+plt.legend(loc='lower right')
 
 # %%
